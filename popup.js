a926d7f549d414d44b13208f10d79242a9b38f01
hhhcbw <79555083+hhhcbw@users.noreply.github.com>
2022-09-22 19:58:56 +0800

    Complete all basic feature.

--- popup.js
+++ popup.js
@@ -1,131 +1,83 @@
 function addTodolist(e) {
   var obj_list = {
-      todo: "",   //用于存储用户输入的数据
-      done: false     //初始化用户输入的数据属性，以便对用户待办事项进行分类
+      todo: "",   // 用于存储用户输入的数据
+      done: false     // false表示未做,true表示已做
   };
-  document.getElementById("add_list").value = document.getElementById("add_list").value.trim();
+  document.getElementById("add_list").value = document.getElementById("add_list").value.trim(); // 去除前后空格
   if (document.getElementById("add_list").value.length === 0){
       alert("不能为空");
       return;
   }
 
-  obj_list.todo = document.getElementById("add_list").value;
-  todolist.push(obj_list);
+  obj_list.todo = document.getElementById("add_list").value; // 用户输入数据
+  todolist.push(obj_list); // 放入TodoList数组中
 
-  saveData(todolist);
+  saveData(todolist); // 保存至本地缓存
 
-  document.getElementById("add_list").value = "";     //初始化输入框
-  load();     //将用户输入的数据添加至dom节点
-  document.getElementById("add_list").focus();
+  document.getElementById("add_list").value = "";     // 初始化输入框
+  load();     // 将用户输入的数据添加至dom节点
+  document.getElementById("add_list").focus(); // 聚焦
 }
 
 function load(){
   var todo = document.getElementById("todolist"),
-      done = document.getElementById("donelist"),
-      todocount = document.getElementById("todocount"),
-      donecount = document.getElementById("donecount"),
-      todoString = "",
-      doneString = "",
-      todoCount = 0,
-      doneCount = 0;
-  document.getElementById("add_list").focus();
+      todoString = "";
 
-  todolist = loadData();
-  console.log(todolist);
-  //todolist数组对象里若包含用户输入数据，则将其添加至dom节点；若为空对象，则初始化页面。
-  if (todolist != null){
-      for (var i=0; i<todolist.length; i ++){
-          if(!todolist[i].done){
-              todoString += "<li>"
-              //通过onchange事件，复选框值有改变则调用update函数，并改变输入数据“done”属性的布尔值，这样
-              //下次load()后，这段数据会进入不同的分组，未完成的事项分入已完成事项组，已完成事项分入未完成事项组
-              //点击事项调用edit函数
-              //点击“-”，调用remove函数
-                  + "<input id='input-"+i+"' type='checkbox' onchange='update("+i+", \"done\", true)'>"
-                  + "<p id='p-"+i+"' onclick='edit("+i+")'>" + todolist[i].todo + "</p>" +
-                  "<a onclick='remove("+i+")'>-</a>" +
-                  "</li>";    //将每次用户输入的数据，通过节点<p>利用id标记，以便后续编辑功能定位
-              // todoString += "<li>"
-              //                 + "<label for='input-" + i + "'>hello</label>"                                
-              //                 + "<input id='input-"+i+"' type='checkbox' onchange='update()'>"
-              //                 + "<p id='p-"+i+"' onclick='edit("+i+")'>" + todolist[i].todo + "</p>" +
-              //                     "<a onclick='remove("+i+")'>-</a>" +
-              //                     "</li>";
-              todoCount ++;
-          }
-          else{
-              doneString += "<li>"
-                  + "<input id='input-" + i + "'type='checkbox' "
-                  + "onchange='update("+i+", \"done\", false)' checked>"
-                  + "<p id='p-"+i+"' onclick='edit("+i+")'>" + todolist[i].todo + "</p>"
-                  + "<a onclick='remove("+i+")'>-</a>"
-                  + "</li>";
-              doneCount ++;
-          }
+  todolist = loadData(); // 从本地缓存中加载数据到todolist中
+  // console.log(todolist);
+
+  // todolist数组对象里若包含用户输入数据，则将其添加至dom节点；若为空对象，则初始化页面。
+  if (todolist.length != 0){
+    for (let i=0; i<todolist.length; i ++){
+      todoString += "<li><input id='input-"+i+"' type='checkbox' onchange='update("+i+",\"done\"," + !todolist[i].done + ")' ";
+        
+      if(todolist[i].done) { // 做过的就打上勾
+          todoString += "checked='checked'";  
       }
-
-      todo.innerHTML = todoString;
-      done.innerHTML = doneString;
-      todocount.innerHTML = todoCount;
-      donecount.innerHTML = doneCount;
+        
+      todoString += " style='display:inline'>"; // 标签不换行，使多个标签可以保持在同一行
+        
+      if(todolist[i].done) { // 做过的打上删除线
+          todoString += "<del style='display:inline'>" + todolist[i].todo + "</del>";
+      } else { // 没做过的不用打上删除线
+          todoString += "<p style='display:inline'>" + todolist[i].todo + "</p>";
+      }
+        
+      todoString += "<a onclick='remove("+i+")'>-</a></li>"; // 删除事项为 - 号
+        
+      todo.innerHTML = todoString; // 替换原有 html 内容
+    }
   }
   else {
-      todo.innerHTML = "";
-      done.innerHTML = "";
-      todocount.innerHTML = 0;
-      donecount.innerHTML = 0;
+      // console.log("empty");
+      todo.innerHTML = ""; // 数组为空直接清空原有 html 内容
   }
-}
-
-function edit(i) {
-  var p = document.getElementById('p-' + i),
-      pContent = p.innerHTML,
-      inputId;
-
-//通过upadate函数对todolist数组相应项进行更新，将用户输入的内容写入到todolist数组相应项的todo属性中
-  function confirm() {
-      if (inputId.value.length === 0) {
-          p.innerHTML = pContent;
-          alert("内容不能为空");
-      }
-      else {
-          update(i, "todo", inputId.value);   //修改事项内容后，更新数组里对应项"todo"属性的值，以便更新dom节点
-      }
-  }
-
-//结合keypress事件，按下enter键，调用confirm函数
-  function enter(e) {
-      if (e.keyCode == 13){
-          confirm();
-      }
-  }
-
-  p.innerHTML = "<input type='text' id='input-"+i+"' value='"+pContent+"'>";
-  inputId = document.getElementById('input-'+i);
-  inputId.focus();
-  inputId.setSelectionRange(0, inputId.value.length);
-  inputId.onblur = confirm;   //表单控件失去焦点，调用confirm函数，即对页面内容进行更新
-  inputId.onkeypress = enter;     //对按键事件进行监控
-}
+} 
 
 function update(i, field, value) {
-  console.log("update");
-  todolist[i][field] = value;
-  saveData(todolist);
-  load();
+  // console.log("update");
+  todolist[i][field] = value; // 字符串形式使用[]，变量形式使用.
+  if (value){ // 如果是已做，则将其放到数组尾部
+    var temp = todolist[i];
+    todolist.splice(i, 1);
+    todolist.push(temp);
+  }
+  else { // 如果变为未做，则将其放到数组头部
+    var temp = todolist[i];
+    todolist.splice(i, 1);
+    todolist.unshift(temp);
+  }
+  
+  saveData(todolist); // 保存至本地缓存
+  load(); // 更新 dom 节点
 }
 
-// function update() {
-//     console.log("update");
-// }
-
 function remove(i) {
-  console.log("remove");
-  todolist.splice(i, 1);
-  
+  // console.log("remove");
+  todolist.splice(i, 1); // splice(i,1) 表示从第i个位置起删除1个元素
   saveData(todolist); //相同名称的缓存会覆盖，更新缓存
 
-  load();
+  load(); // 更新 dom 节点
 }
 
 function saveData(data) {
@@ -133,22 +85,46 @@ function saveData(data) {
 }
 
 function loadData() {
-  var hisTory = localStorage.getItem("mytodolist");
+  var hisTory = localStorage.getItem("mytodolist"); // 从本地缓存获取 mytodolist 对应项的JSON对象
+  // console.log(hisTory)
   if(hisTory !=null){
-      return JSON.parse(hisTory);     //JSON对象转换为JS对象
+      return JSON.parse(hisTory);     // JSON对象转换为JS对象
   }
   else { return []; }
 }
 
 function clear() {
-  localStorage.clear();
-  load();
+  localStorage.removeItem("mytodolist"); // 删除本地缓存的 mytodolist 项
+  load(); // 更新 dom 节点
+}
+
+function deletedone() {
+  // 找到最小的已做事项的位置，然后从这个位置开始删除
+  var pos = todolist.length;
+  for(let i=0; i<todolist.length; i ++) { // let 是块作用域 var 是函数作用域
+    if (todolist[i].done) {
+      pos = i;
+      break;
+    }
+  }
+  todolist.splice(pos);
+  saveData(todolist); // 保存至本地缓存
+
+  load(); // 更新 dom 节点
 }
 
 window.addEventListener("load", load);  //页面加载完毕调用load函数
+
+// 清除全部
 document.getElementById("clearbutton").onclick = clear;
-document.getElementById("add_list").onkeypress = function (event) {
-  if(event.keyCode === 13){
+// 删除已做
+document.getElementById("deletedonebutton").onclick = deletedone;
+
+// 回车添加
+document.getElementById("add_list").onkeydown = function (event) {
+  if(event.key === 'Enter'){
       addTodolist();
   }
 };
+// 点击按钮添加
+document.getElementById("addbutton").onclick = addTodolist;
